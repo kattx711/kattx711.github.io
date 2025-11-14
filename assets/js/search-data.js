764bdc4cfccf4c2e215c9a76cea0defbb0e59186
kattx711 <151250837+kattx711@users.noreply.github.com>
2025-11-14 02:15:29 +0000

    Deploying to gh-pages from @ kattx711/kattx711.github.io@d7e2a7a4504b29c2b2715ac70b168cdda0da2029 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "CV",
           description: "Full curriculum vitae for Katherine Avanesov.",
           section: "Navigation",
